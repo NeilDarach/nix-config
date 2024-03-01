60a6845835a2d04addb536ee899d9cd125857577
Neil Darach <neil.darach@gmail.com>
2024-03-01 15:50:12 +0000

    paths

--- profiles/standard/configuration.nix
+++ profiles/standard/configuration.nix
@@ -5,6 +5,10 @@
     ../../system/bluetooth.nix
     ];
 
+  nix.nixPath = [ "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
+                  "nixos-config=$HOME/dotfiles/system/configuration.nix"
+		  "/nix/var/nix/profiles/per-user/root/channels"
+		  ];
   nix.package = pkgs.nixFlakes;
   nix.extraOptions = ''
     experimental-features = nix-command flakes
