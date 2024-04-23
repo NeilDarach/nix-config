8f9c5ebfaaea4c9a5bc7b6eb4821764d5fc7b709
Neil Darach <neil.darach@gmail.com>
2024-04-23 21:37:34 +0100

    Made the homeasisstant flake a little more generic

--- hosts/common/optional/podman/homeassistant.nix
+++ hosts/common/optional/podman/homeassistant.nix
@@ -6,6 +6,8 @@
 }: let
   inherit (lib) mkIf;
   enabled = config.localServices.homeassistant.enable;
+  baseDir = "/services/homeassistant";
+  pool = config.networking.hostName;
 in {
   config = mkIf enabled {
     networking.firewall.allowedTCPPorts = [8123];
@@ -19,7 +21,7 @@ in {
               "TZ" = "Europe/London";
             };
             volumes = [
-              "/home/neil/ha:/config"
+              "${baseDir}:/config"
               "/run/dbus:/run/dbus:ro"
             ];
             extraOptions = [
@@ -41,10 +43,10 @@ in {
         Type = "oneshot";
       };
       script = ''
-        mkdir -p /services/homeassistant
-        ${pkgs.zfs}/bin/zfs list pi400/services/homeassistant || ${pkgs.zfs}/bin/zfs create -p -u -o mountpoint=legacy pi400/services/homeassistant
-        [[ -f /services/homeassistant/mounted.txt ]] || ${pkgs.util-linux}/bin/mount -t zfs pi400/services/homeassistant /services/homeassistant
-        echo > /services/homeassistant/mounted.txt
+        mkdir -p ${baseDir}
+        ${pkgs.zfs}/bin/zfs list ${pool}${baseDir} || ${pkgs.zfs}/bin/zfs create -p -u -o mountpoint=legacy ${pool}${baseDir}
+        [[ -f ${baseDir}/mounted.txt ]] || ${pkgs.util-linux}/bin/mount -t zfs ${pool}${baseDir} ${baseDir}
+        echo > ${baseDir}/mounted.txt
       '';
     };
 
