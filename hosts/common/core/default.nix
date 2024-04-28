458d58c3060171421bf1dd68b84bb7540e61b781
Neil Darach <neil.darach@gmail.com>
2024-04-28 18:48:31 +0100

    Modify the linuxserver.io docker container to be able to accept bluetooth BLE advertisments. Set a capability on the python executable and install aioblescan and janus. Future work to pull the whl files down at build time instead of keeping them in the repository.

--- hosts/common/core/default.nix
+++ hosts/common/core/default.nix
@@ -1,36 +1,47 @@
-{ 
+{
   inputs,
   outputs,
   pkgs,
   ...
-  } : {
-  imports = [ 
-    inputs.home-manager.nixosModules.home-manager
-    ./fish.nix
-    ./sops.nix
-    ./distributed-build.nix
-    ./optin-persistence.nix
-    ./openssh.nix
-    ./security.nix
-    ./locale.nix
-    ./nix.nix
-    ] ++ (builtins.attrValues outputs.nixosModules);
+}: {
+  imports =
+    [
+      inputs.home-manager.nixosModules.home-manager
+      ./fish.nix
+      ./sops.nix
+      ./distributed-build.nix
+      ./optin-persistence.nix
+      ./openssh.nix
+      ./security.nix
+      ./locale.nix
+      ./nix.nix
+    ]
+    ++ (builtins.attrValues outputs.nixosModules);
 
- environment.systemPackages = builtins.attrValues {
-    inherit (pkgs)
-      just; };
+  environment.systemPackages = builtins.attrValues {
+    inherit
+      (pkgs)
+      just
+      wget
+      ;
+  };
 
+  users.groups = {
+    bluetooth = {};
+  };
+  services.udev.extraRules = ''
+    KERNEL=="rfkill", SUBSYSTEM=="misc", GROUP="bluetooth", MODE="0664"
+  '';
   home-manager.extraSpecialArgs = {
     inherit inputs outputs;
-    };
+  };
 
   nixpkgs = {
     overlays = builtins.attrValues outputs.overlays;
     config = {
       allowUnfree = true;
-      };
     };
+  };
 
   networking.domain = "darach.org.uk";
-  }
-
+}
--- hosts/common/optional/podman/homeassistant.nix
+++ hosts/common/optional/podman/homeassistant.nix
@@ -16,6 +16,32 @@
     finalImageName = "linuxserver/homeassistant";
     finalImageTag = "2024.4.4";
   };
+
+  ha = pkgs.dockerTools.buildImage {
+    name = "local/homeassistant";
+    tag = "local";
+    fromImage = img;
+    copyToRoot = pkgs.buildEnv {
+      name = "homeassistant-pips";
+      paths = [./homeassistant-pip];
+    };
+    #    runAsRoot = ''
+    #!/bin/bash
+    #      export PATH=/lsiopy/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+    #      mkdir -p /etc/s6-overlay/s6-rc.d/setBluetoothOneshot
+    #      echo "oneshot" > /etc/s6-overlay/s6-rc.d/setBluetoothOneshot/type
+    #      echo "foreground { echo 'Setting bluetooth capabilities' } " > /etc/s6-overlay/s6-rc.d/setBluetoothOneshot/up
+    #      echo "/homeassistant-pip/setBluetoothCapabilities.sh" >> /etc/s6-overlay/s6-rc.d/setBluetoothOneshot/up
+    #      touch /etc/s6-overlay/s6-rc.d/user/contents.d/setBluetoothOneshot
+    #    '';
+    config = {
+      Cmd = ["/init"];
+      WorkingDir = "/config";
+    };
+    diskSize = 5120;
+    buildVMMemorySize = 1024;
+  };
 in {
   options = {
     localServices.homeassistant.pool = lib.mkOption {
@@ -34,7 +60,7 @@ in {
     users.users.homeassistant = {
       uid = 8123;
       group = "homeassistant";
-      extraGroups = ["systemd-journal"];
+      extraGroups = ["systemd-journal" "bluetooth"];
       createHome = false;
       home = "/services/homeassistant";
       isNormalUser = true;
@@ -49,8 +75,8 @@ in {
         backend = "podman";
         containers = {
           homeassistant = {
-            image = "linuxserver/homeassistant:2024.4.4";
-            imageFile = img;
+            image = "local/homeassistant:local";
+            imageFile = ha;
             environment = {
               "TZ" = "Europe/London";
               "PUID" = "8123";
@@ -58,10 +84,12 @@ in {
             };
             volumes = [
               "${baseDir}:/config"
-              "/run/dbus:/run/dbus:ro"
+              "/var/run/dbus:/var/run/dbus:ro"
+              "/dev/rfkill:/dev/rfkill:rw"
             ];
             extraOptions = [
               "--name=homeassistant"
+              #"--privileged"
               "--network=host"
               "--cap-add=NET_ADMIN"
               "--cap-add=NET_RAW"
@@ -87,6 +115,8 @@ in {
       '';
     };
 
-    systemd.services.podman-homeassistant.after = ["prepare-homeassistant.service"];
+    systemd.services.podman-homeassistant = {
+      after = ["prepare-homeassistant.service"];
+    };
   };
 }
