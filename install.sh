ed77b3822cfce872b3d2e0bfa55889b49f3361b1
Neil Darach <neil.darach@gmail.com>
2024-10-18 09:43:02 +0100

    Start with an empty directory

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh 
-
-nix-shell -p git --command "git clone https://github.com/NeilDarach/nix-config ~/.dotfiles"
-sudo nixos-generate-config --show-hardware-config > ~/.dotfiles/system/hardware-configuration.nix
-
-sudo nixos-rebuild switch --flake ~/.dotfiles#system;
-nix run home-manager/master --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake ~/.dotfiles#user;
-
-
