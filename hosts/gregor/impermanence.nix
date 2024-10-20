3b6dbe9b19c6e042cc2ed3d7636dd32ff1e28648
Neil Darach <neil.darach@gmail.com>
2024-10-20 08:27:25 +0100

    Actually include the persistence section for impermanance

--- hosts/gregor/impermanence.nix
+++ hosts/gregor/impermanence.nix
@@ -2,8 +2,13 @@
   system.activationScripts.createPersist = "mkdir -p /persist";
   environment.persistence."/persist" = {
     hideMounts = true;
-    directories = [ "/etc/nixos" "/etc/NetworkManager" "/var/lib/bluetooth" ];
+    directories = [
+      "/etc/nixos"
+      "/etc/NetworkManager"
+      "/var/lib/bluetooth"
+      "/var/lib/nixos"
+    ];
 
-  };
     files = [ "/etc/machine-id" ];
+  };
 }
