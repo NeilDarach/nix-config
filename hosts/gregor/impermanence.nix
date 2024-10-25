92e1c315362607e2489bb20a49f503dea4359bcb
Neil Darach <neil.darach@gmail.com>
2024-10-25 15:00:18 +0100

    Running zigbee2mqtt as a service.  Connected to the uzg-01 remote coordinator, with secrets in sops.

--- hosts/gregor/impermanence.nix
+++ hosts/gregor/impermanence.nix
@@ -6,9 +6,25 @@
       "/etc/nixos"
       "/etc/NetworkManager"
       "/var/lib/bluetooth"
-      "/var/lib/plex"
       "/var/lib/nixos"
-      "/var/lib/transmission"
+      {
+        directory = "/var/lib/plex";
+        user = "plex";
+        group = "plex";
+        mode = "u=rwx,g=rx,o=rx";
+      }
+      {
+        directory = "/var/lib/transmission";
+        user = "transmission";
+        group = "transmission";
+        mode = "u=rwx,g=rx,o=rx";
+      }
+      {
+        directory = "/var/lib/zigbee2mqtt";
+        user = "zigbee2mqtt";
+        group = "zigbee2mqtt";
+        mode = "u=rwx,g=rx,o=rx";
+      }
     ];
 
     files = [ "/etc/machine-id" ];
