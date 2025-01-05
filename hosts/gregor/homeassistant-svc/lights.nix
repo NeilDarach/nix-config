38b0c095aaf6e26b14edec2bf35df3c47e59f043
Neil Darach <neil.darach@gmail.com>
2025-01-05 14:20:30 +0000

    Add an explicit automation

--- hosts/gregor/homeassistant-svc/lights.nix
+++ hosts/gregor/homeassistant-svc/lights.nix
@@ -5,6 +5,28 @@
     }) ++ (ha.lightBrightness "TestDimLights" 100 {
       transition = 5;
       entities = [ "light.livingroom_lamp_left" "light.livingroom_lamp_right" ];
-    });
+    }) ++ [{
+      alias = "Explicit Automation";
+      description = "Long form in light.nix";
+                trigger = [];
+                condition = [];
+      action = [
+        {
+          service = "light.turn_on";
+          data = {
+            transition = 5;
+            color_temp = 153;
+          };
+          target.entity_id = "light.livingroom_lamp_left";
+        }
+        { delay.seconds = 3; }
+        {
+          service = "light.turn_on";
+          data.color_temp = 500;
+          target.entity_id = "light.livingroom_lamp_left";
+        }
+      ];
+      mode = "single";
+    }];
   };
 }
