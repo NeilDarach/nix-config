feaf464a6debade617ed3ff4d45f4182246583a3
Neil Darach <neil.darach@gmail.com>
2025-01-15 06:47:29 +0000

    Made this zigbee2mqtt the primary.  Changed the channel back to 11, and the mqtt topic to default.  Secrets provided in env vars

--- hosts/gregor/zigbee2mqtt.nix
+++ hosts/gregor/zigbee2mqtt.nix
@@ -1,48 +1,43 @@
-{ pkgs, config, outputs, ... }: {
-  environment.persistence."/persist".directories = [{
-    directory = "/var/lib/zigbee2mqtt";
-    user = "zigbee2mqtt";
-    group = "zigbee2mqtt";
-    mode = "u=rwx,g=rx,o=rx";
-  }];
+{ pkgs, config, outputs, ... }:
+let utils = import ../../lib/svcUtils.nix;
+in {
 
   sops.secrets.mqtt-user = { restartUnits = [ "zigbee2mqtt.service" ]; };
   sops.secrets.mqtt-password = { restartUnits = [ "zigbee2mqtt.service" ]; };
 
   sops.templates."z2m-secret.yaml" = {
     content = ''
-      user: ${config.sops.placeholder.mqtt-user}
-      password: ${config.sops.placeholder.mqtt-password}
+      ZIGBEE2MQTT_CONFIG_MQTT_USER=${config.sops.placeholder.mqtt-user}
+      ZIGBEE2MQTT_CONFIG_MQTT_PASSWORD=${config.sops.placeholder.mqtt-password}
     '';
-    path = "/var/lib/zigbee2mqtt/secret.yaml";
     owner = "zigbee2mqtt";
   };
   networking.firewall.allowedTCPPorts = [ 8080 ];
 
+  users.users.neil.extraGroups = [ "zigbee2mqtt" ];
+
+  systemd.timers.strongStateDir-backup-zigbee2mqtt =
+    (utils.zfsBackup "zigbee2mqtt" "zigbee2mqtt");
+  services.strongStateDir.enable = true;
   services.zigbee2mqtt = {
     enable = true;
-    dataDir = "/var/lib/zigbee2mqtt";
+    dataDir = "/strongStateDir/zigbee2mqtt";
     settings = {
       homeassistant = true;
       permit_join = true;
       mqtt = {
-        base_topic = "uzg01";
+        base_topic = "zigbee2mqtt";
         server = "mqtt://mqtt.darach.org.uk";
-        user = "!secret.yaml user";
-        password = "!secret.yaml password";
       };
 
-      secret = "${config.sops.templates."z2m-secret.yaml".path}";
-      serial = {
-        port = "tcp://192.168.4.233:6638";
-        baudrate = 115200;
-      };
+      serial = { port = "/dev/ttyUSB0"; };
       advanced = {
         homeassistant_legacy_entity_attributes = false;
         homeassistant_status_topic = "homeassistant/status";
         homeassistant_discovery_topic = "homeassistant";
         legacy_api = false;
         ikea_ota_use_test_url = false;
+        log_directory = "/var/log/zigbee2mqtt/%TIMESTAMP%";
         log_syslog = {
           app_name = "Zigbee2MQTT";
           eol = "/n";
@@ -55,8 +50,7 @@
           type = "5424";
 
         };
-        channel = 25;
-        log_level = "debug";
+        log_level = "info";
         legacy_availability_payload = true;
         last_seen = "ISO_8601_local";
         transmit_power = 10;
@@ -73,13 +67,21 @@
   systemd.services.zigbee2mqtt = {
     enable = true;
     serviceConfig = {
+      UMask = pkgs.lib.mkForce "0007";
+      StateDirectoryMode = "0770";
+      WorkingDirectoryMode = "0770";
+      LogsDirectory = "zigbee2mqtt";
+      EnvironmentFile = "${config.sops.templates."z2m-secret.yaml".path}";
       ExecStartPost = [
         ''
-          +${pkgs.registration}/bin/registration zigbee2mqtt-uzg 192.168.4.5 8080 "zigbee2mqtt running from the UZG-01"''
+          +${pkgs.registration}/bin/registration zigbee2mqtt 192.168.4.5 8080 "zigbee2mqtt"''
       ];
       ExecStop = [ "+rm /var/run/registration-leases/zigbee2mqtt" ];
     };
-    wants = [ "registration.timer" ];
+    wants = [
+      "registration.timer"
+      "strongStateDir@zigbee2mqtt:zigbee2mqtt:zigbee2mqtt:zigbee2mqtt.service"
+    ];
   };
 
 }
