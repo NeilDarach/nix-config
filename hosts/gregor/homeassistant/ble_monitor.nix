55770e30118b36019bbc9d9406af17395b2fee15
Neil Darach <neil.darach@gmail.com>
2025-12-12 20:08:29 +0000

    Newest version of the ble monitor

--- hosts/gregor/homeassistant/ble_monitor.nix
+++ hosts/gregor/homeassistant/ble_monitor.nix
@@ -3,21 +3,20 @@
 buildHomeAssistantComponent rec {
   owner = "custom-components";
   domain = "ble_monitor";
-  version = "12.20.0";
-
+  version = "13.10.1";
 
   src = fetchFromGitHub {
     owner = "custom-components";
     repo = "ble_monitor";
     rev = "${version}";
-    sha256 = "sha256-dDiypaOqV6n6duJUWLxlfjNHOYUnNRsV6srJuSHlZns=";
+    sha256 = "sha256-MwYCQDc9cIy5lYcU+p8rKGeAXrMJjg+LTyafCveBcDI=";
   };
 
-  propagatedBuildInputs =  [ 
+  propagatedBuildInputs = [
     home-assistant.python.pkgs.pycryptodomex
     home-assistant.python.pkgs.janus
     home-assistant.python.pkgs.aioblescan
     home-assistant.python.pkgs.btsocket
     home-assistant.python.pkgs.pyric
-    ];
+  ];
 }
