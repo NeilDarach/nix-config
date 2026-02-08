3505bf720cad05be171984ddc2f54af6bbda4017
Neil Darach <neil.darach@gmail.com>
2026-02-08 12:27:24 +0000

    bridge and vlan with static ipv4 addresses

--- modules/common/common.nix
+++ modules/common/common.nix
@@ -86,6 +86,9 @@
         networking.firewall.enable = true;
         networking.enableIPv6 = false;
         networking.networkmanager.enable = true;
+        networking.hostId = lib.mkDefault (
+          builtins.substring 0 8 (builtins.hashString "md5" config.networking.hostName)
+        );
         time.timeZone = "Europe/London";
 
         security.sudo = {
--- modules/hosts/r5s.nix
+++ modules/hosts/r5s.nix
@@ -37,12 +37,102 @@ in
         jellyfin.enable = true;
       };
 
-      networking = {
-        hostName = "r5s";
-        useDHCP = lib.mkForce true;
-        hostId = "d9165aff";
+      networking.hostName = "r5s";
+      networking.useDHCP = false;
+      systemd.network = {
+        enable = true;
+        netdevs = {
+          "10-br0" = {
+            netdevConfig = {
+              Kind = "bridge";
+              Name = "br0";
+            };
+          };
+          "20-vlan5" = {
+            netdevConfig = {
+              Kind = "vlan";
+              Name = "vlan5";
+            };
+            vlanConfig.Id = 5;
+          };
+        };
+        networks = {
+          "30-lan1" = {
+            matchConfig.Name = "lan1";
+            networkConfig.Bridge = "br0";
+            linkConfig.RequiredForOnline = "enslaved";
+          };
+          "30-lan2" = {
+            matchConfig.Name = "lan2";
+            networkConfig.Bridge = "br0";
+            linkConfig.RequiredForOnline = "enslaved";
+          };
+          "40-br0" = {
+            matchConfig.Name = "br0";
+            bridgeConfig = { };
+            vlan = [ "vlan5" ];
+            address = [ "192.168.4.2/24" ];
+            routes = [ { Gateway = "192.168.4.1"; } ];
+            networkConfig = {
+              DHCP = "no";
+              IPv6AcceptRA = false;
+            };
+            linkConfig.RequiredForOnline = "routable";
+          };
+          "40-vlan5" = {
+            matchConfig.Name = "vlan5";
+            address = [ "192.168.5.2/24" ];
+            routes = [ { Gateway = "192.168.5.1"; } ];
+            networkConfig = {
+              DHCP = "no";
+              IPv6AcceptRA = false;
+            };
+            linkConfig.RequiredForOnline = "routable";
+          };
+        };
       };
-      system.stateVersion = lib.mkDefault "25.11";
 
+      /*
+          useDHCP = lib.mkForce false;
+          interfaces = {
+            lan1.useDHCP = false;
+            lan2.useDHCP = false;
+            br0 = {
+              useDHCP = false;
+              ipv4.addresses = [
+                {
+                  address = "192.168.4.2";
+                  prefixLength = 24;
+                }
+              ];
+            };
+            vlan5 = {
+              useDHCP = false;
+              ipv4.addresses = [
+                {
+                  address = "192.168.5.2";
+                  prefixLength = 24;
+                }
+              ];
+            };
+          };
+
+          bridges = {
+            "br0" = {
+              interfaces = [
+                "lan1"
+                "lan2"
+              ];
+            };
+          };
+          vlans = {
+            vlan5 = {
+              id = 5;
+              interface = "br0";
+            };
+          };
+        };
+      */
+      system.stateVersion = lib.mkDefault "25.11";
     };
 }
