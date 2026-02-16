70f3f25e43a990ab6da3fb5caf1004b32182690d
Neil Darach <neil.darach@gmail.com>
2026-02-16 10:50:02 +0000

    Disable rsa ssh keys

--- modules/common/ssh.nix
+++ modules/common/ssh.nix
@@ -10,12 +10,20 @@
       { config, ... }:
       {
         config = lib.mkIf config.services.openssh.enable {
-          services.openssh.settings = {
-            PermitRootLogin = "no";
-            PasswordAuthentication = false;
-            StreamLocalBindUnlink = "yes";
-            GatewayPorts = "clientspecified";
-            AllowUsers = [ "neil" ];
+          services.openssh = {
+            hostKeys = lib.mkForce [
+              {
+                type = "ed25519";
+                path = "/etc/ssh/ssh_host_ed25519_key";
+              }
+            ];
+            settings = {
+              PermitRootLogin = "no";
+              PasswordAuthentication = false;
+              StreamLocalBindUnlink = "yes";
+              GatewayPorts = "clientspecified";
+              AllowUsers = [ "neil" ];
+            };
           };
         };
       };
